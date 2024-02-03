cd125d35239c0f374c4bfb54137d83310b479fdc
Elva Hadi <codacrafts.dev@gmail.com>
2024-02-03 17:57:18 +0800

    fix build error

--- commands.go
+++ commands.go
@@ -1,36 +1,11 @@
 package cli
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"os/exec"
-	"regexp"
 )
 
-func version() string {
-	file, err := os.Open("changelog.md")
-	if err != nil {
-		return ""
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	versionRegex := regexp.MustCompile(`## \[([0-9]+\.[0-9]+\.[0-9]+)\]`)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if matches := versionRegex.FindStringSubmatch(line); matches != nil {
-			return matches[1]
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return ""
-	}
-
-	return "version not found"
-}
+var version = "0.1.1"
 
 func about() {
 	fmt.Println(`                               
@@ -45,7 +20,7 @@ func about() {
      .       .                     .-    .                  .  #%:  *                 
                                                             .  -.  *                  
                                                              .   . `)
-	fmt.Printf("%s%s%s", "Callamary", "\033[32m version \033[37m", version()+".\r\nDeveloped by \033[32mElva Hadi Prajatama \033[36m@codacrafts\033[37m\n\n")
+	fmt.Printf("%s%s%s", "Callamary", "\033[32m version \033[37m", version+".\r\nDeveloped by \033[32mElva Hadi Prajatama \033[36m@codacrafts\033[37m\n\n")
 }
 
 func build() {
