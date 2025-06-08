8b854895d14d18c196bf78f00c148f393656f7de
codacrafts <codacrafts.dev@gmail.com>
2025-06-08 13:50:53 +0800

    bump go version

--- commands.go
+++ commands.go
@@ -7,20 +7,20 @@ import (
 	"path/filepath"
 )
 
-var version = "2.0.0"
+var version = "2.0.2"
 
 func about() {
-	fmt.Println(`                               
-       *. :         @..:      -          -      .                                     
-    :        .              .                                                         
-   .  :+##*=  :   .  +* .#=  .   .         :   .  .   :   .  .  - .  -                
-  .  *%*---=   ...   #@..@+   ..   ..  . .  ...    ..    . ..      .  .               
-    -@+       =###*. #%..@+  +###+  +%###*##%+  +###= -%##* *#. =%. .               
-    :@*.      .--+@= #%..@+  :--*@. #@- *@- +@: :--#@.=@+   :%#-%*  :               
-  :  -%%*+*#- #%=#@= #@..@* .%#=%@: #@: *@: +@:.%#+%@.=@=    :%@%. .                
-   :   -=+=:  :==-+. -=  =:  -+=-=  -=  :+. :=. -+=-= .+.  .  *%-                   
-     .       .                     .-    .                  .  #%:  *                 
-                                                            .  -.  *                  
+	fmt.Println(`
+       *. :         @..:      -          -      .
+    :        .              .
+   .  :+##*=  :   .  +* .#=  .   .         :   .  .   :   .  .  - .  -
+  .  *%*---=   ...   #@..@+   ..   ..  . .  ...    ..    . ..      .  .
+    -@+       =###*. #%..@+  +###+  +%###*##%+  +###= -%##* *#. =%. .
+    :@*.      .--+@= #%..@+  :--*@. #@- *@- +@: :--#@.=@+   :%#-%*  :
+  :  -%%*+*#- #%=#@= #@..@* .%#=%@: #@: *@: +@:.%#+%@.=@=    :%@%. .
+   :   -=+=:  :==-+. -=  =:  -+=-=  -=  :+. :=. -+=-= .+.  .  *%-
+     .       .                     .-    .                  .  #%:  *
+                                                            .  -.  *
                                                              .   . `)
 	fmt.Printf("%s%s%s", "Callamary", "\033[32m version \033[37m", version+".\r\nDeveloped by \033[32mElva Hadi Prajatama \033[36m@codacrafts\033[37m\n\n")
 }
@@ -28,13 +28,13 @@ func about() {
 func build() {
 	cwd, _ := os.Getwd()
 
-	fmt.Println("Use default source location? (y/n): ")
+	fmt.Println("Use default source location /app/cli/main.go? (y/n): ")
 	var response string
 	fmt.Scanln(&response)
 
 	var loc string
 	if response == "y" {
-		path := "/cli/main.go"
+		path := "/app/cli/main.go"
 		loc = filepath.Join(cwd, path)
 	} else {
 		var custom string
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/callamary/cli/v2
 
-go 1.22
+go 1.24.4
